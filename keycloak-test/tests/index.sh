1441a14cda8ba057e32658f2c6238886bb7dc915
Tilman Kranz <tilman.kranz@b1-systems.de>
2026-06-05 07:57:50 +0200

    implement healthcheck for keycloak-test based on overall test status

--- keycloak-test/tests/index.sh
+++ keycloak-test/tests/index.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+# Find all test scripts and execute them in lexical order of script name.
+
+# shellcheck disable=SC2034
 
 dir=$(readlink -f "$(dirname "$0")")
 
 while read -r script ; do
-    echo "### RUNNING TEST: $script ..."
-    "$script"
+    echo "### RUNNING TESTS: $script ..." >&2
+    if ! "$script" ; then
+        echo "### SOME OR ALL TESTS FAILED: $script" >&2
+        echo failed > /result
+    else
+        echo "### ALL TESTS SUCCEEDED: $script" >&2
+        echo succeeded > /result
+    fi
 done < <(find "$dir" -name 'custom-*.sh')
+
+read -r -p "Entering idle mode ..." noreply
